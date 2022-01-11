baaa10af5056b3f0d4873965eff2941634273d6f
Nils Ponsard <nilsponsard@gmail.com>
2022-01-11 09:27:15 +0100

    connect transition to no orm

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,52 +1,45 @@
-import { Database, PostgresConnector } from "https://deno.land/x/denodb@v1.0.40/mod.ts";
+import { Client } from "https://deno.land/x/postgres/mod.ts";
 
-import { Link } from "./link.ts";
-
-export default function Connect(
+export default async function Connect(
   database: string,
-  host: string,
-  username: string,
+  hostname: string,
+  user: string,
   password: string,
   port: number
 ) {
-  const connector = new PostgresConnector({
+  const client = new Client({
     database,
-    host,
-    username,
+    hostname,
+    user,
     password,
     port,
   });
+  await client.connect();
 
-  const db = new Database(connector);
-
-  Link(db);
-
-  db.sync();
-
-  return db;
+  return client;
 }
 
-
 // connect to database with defaults/env vars
-export function ConnectDefaults() {
+export async function ConnectDefaults() {
   const DB_HOST = Deno.env.get("DB_HOST") || "localhost";
   const DB_NAME = Deno.env.get("DB_NAME") || "tournament";
   const DB_USERNAME = Deno.env.get("DB_USERNAME") || "postgres";
   const DB_PASSWORD = Deno.env.get("DB_PASSWORD") || "postgres";
-  
-  
+
   let DB_PORT = 5432;
 
   const port = Deno.env.get("DB_PORT");
   if (port && !isNaN(parseInt(port))) {
     DB_PORT = parseInt(port);
   }
-  
 
-  const db = Connect(DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_PORT);
-  
-  // TODO : Add triggers 
-  
-  // db.query("");
-  return db
+  const client = await Connect(
+    DB_NAME,
+    DB_HOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_PORT
+  );
+
+  return client;
 }
