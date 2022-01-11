c1a505400cae6c3c8d6ed8cf94c18d73d24cf106
Nils Ponsard <nilsponsard@gmail.com>
2022-01-11 10:35:25 +0100

    init db with env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import Start from "./api/start.ts";
+import { ConnectDefaults } from "./database/connect.ts";
+import Init from "./database/init.ts";
+
+const INIT_DB = Deno.env.get("INIT_DB") || "false";
+
+const pool = await ConnectDefaults();
+
+if (INIT_DB === "true") {
+  await Init(pool);
+}
 
 await Start();
