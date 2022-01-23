58af93d147c0ad8e2d0a7b0e9e62ba153161d4dc
NilsPonsard <nilsponsard@gmail.com>
2022-01-23 10:58:11 +0100

    update api definition

--- src/database/entities/tournaments.ts
+++ src/database/entities/tournaments.ts
@@ -11,8 +11,9 @@ export enum TournamentType {
 
 export enum TournamentStatus {
     Created = 0,
-    Started = 1,
-    Finished = 2,
+    Generated = 1,
+    Started = 2,
+    Finished = 3,
 }
 
 export interface Tournament {
