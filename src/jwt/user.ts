ac3529876dec98c3a3dca762681ddb9d0fad46b4
Nils Ponsard <nilsponsard@gmail.com>
2022-01-31 14:19:48 +0100

    return null on Jwt error

--- src/jwt/user.ts
+++ src/jwt/user.ts
@@ -3,29 +3,30 @@ import { GetTokensWithAccessToken } from "../database/entities/token.ts";
 import { GetUser } from "../database/entities/user.ts";
 import { DecodeJWT } from "./signature.ts";
 
-
-export async function GetUserWithAccessToken(
-    pool: Pool,
-    accessJWT: string | null
-) {
+export async function GetUserWithAccessToken(pool: Pool, accessJWT: string | null) {
     if (!accessJWT) return null;
 
-    // decode JWT
-    const decoded = await DecodeJWT(accessJWT);
-    if (!decoded) return null;
+    try {
+        // decode JWT
+        const decoded = await DecodeJWT(accessJWT);
+        if (!decoded) return null;
 
-    // check if token is valid in database
-    const tokens = await GetTokensWithAccessToken(pool, decoded.token);
-    if (
-        !tokens ||
-        tokens.user_id != decoded.id ||
-        tokens.expiration.getTime() < Date.now() /* invalid if expired  */
-    )
+        // check if token is valid in database
+        const tokens = await GetTokensWithAccessToken(pool, decoded.token);
+        if (
+            !tokens ||
+            tokens.user_id != decoded.id ||
+            tokens.expiration.getTime() < Date.now() /* invalid if expired  */
+        )
+            return null;
+
+        // get the user
+        const user = await GetUser(pool, tokens.user_id);
+        if (!user) return null;
+
+        return user;
+    } catch (e) {
+        console.log(e);
         return null;
-
-    // get the user
-    const user = await GetUser(pool, tokens.user_id);
-    if (!user) return null;
-
-    return user;
+    }
 }
