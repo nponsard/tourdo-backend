0b9997c8cd7197ec8ea423be320bd076bec22a22
Nils Ponsard <nilsponsard@gmail.com>
2022-02-01 16:58:46 +0100

    fix truncate

--- demo/sample_data.sql
+++ demo/sample_data.sql
@@ -1,5 +1,5 @@
-id,truncate tokens;
-truncate users;
+truncate tokens cascade ;
+truncate users cascade ;
 
 insert into  users (id,username, password, admin) values (1,'admin','$2a$10$HWR6HocjushAop9hjO/iIOOBWWyoN/IsGpK0CxTmqzDEgINJt8Req', true);
 insert into  users (id,username, password, admin) values (2,'user1','$2a$10$HWR6HocjushAop9hjO/iIOOBWWyoN/IsGpK0CxTmqzDEgINJt8Req', false);
@@ -8,33 +8,33 @@ insert into  users (id,username, password, admin) values (4,'user3','$2a$10$HWR6
 insert into  users (id,username, password, admin) values (5,'user4','$2a$10$HWR6HocjushAop9hjO/iIOOBWWyoN/IsGpK0CxTmqzDEgINJt8Req', false);
 
 
-truncate teams;
+truncate teams cascade ;
 
 insert into teams (id, name, description) values (1,'team1','user1’s team');
 insert into teams (id, name, description) values (2,'team2','user2’s team');
 insert into teams (id, name, description) values (3,'team3','user3’s team');
 insert into teams (id, name, description) values (4,'team4','user4’s team');
 
-truncate teams_composition;
+truncate teams_composition cascade ;
 
 insert into teams_composition (user_id, team_id, role) values (2,1,1);
 insert into teams_composition (user_id, team_id, role) values (3,2,1);
 insert into teams_composition (user_id, team_id, role) values (4,3,1);
 insert into teams_composition (user_id, team_id, role) values (5,4,1);
 
-truncate tournaments;
+truncate tournaments cascade ;
 
 insert into tournaments (id, type, name, description, status, max_teams, game_name) values (1,2,'Test tournament', 'demo tournament', 0,4,'test game');
 
-truncate tournaments_organizers;
+truncate tournaments_organizers cascade ;
 
 insert into tournaments_organizers (tournament_id, user_id) VALUES (1,1);
 
-truncate tournaments_participants;
+truncate tournaments_participants cascade ;
 
 insert into tournaments_participants (team_id, team_number) values (1,1);
 insert into tournaments_participants (team_id, team_number) values (2,2);
 insert into tournaments_participants (team_id, team_number) values (3,3);
 insert into tournaments_participants (team_id, team_number) values (4,4);
 
-truncate matches;
+truncate matches cascade ;
